612d1fbfb9eb6b04d11f151d052d03cf54dc25ae
LeonardoLiberato <139288816+LeonardoLiberato@users.noreply.github.com>
2023-07-11 20:39:38 -0300

    Update TRANSFORMANDO (C) EM (F).py

--- TRANSFORMANDO (C) EM (F).py
+++ TRANSFORMANDO (C) EM (F).py
@@ -1,4 +1,3 @@
-#Desafio 14
 #escreva um programa que converte uma temperatura digitada em C e converta para F
 #
 #exercicio 1
@@ -14,11 +13,4 @@ ce = float(input("Informe a temperatura em C"))
 fa = ((9*ce)/5)+32
 print('A temperatura de {}C corresponde a {}F.'.format(ce,fa))
 
-#etapas de calculo de C para F
-#9xC = resultado
-#resultado dividido por 5
-#resultado da divisao =32
-#
-# 9 x 36.8c = 331.2
-# 331.2 / 5 = 66.24
-# 66.24 + 32 =98.24
+
